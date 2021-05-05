0d1fcf8df8bd6c13ec49e7ca3cd04de546109f5c
yohance-lorrington <tabahanih@gmail.com>
2021-05-05 11:58:04 -0400

    Update BDM_HW4_Hayles.py

--- BDM_HW4_Hayles.py
+++ BDM_HW4_Hayles.py
@@ -12,6 +12,12 @@ import sys
 def calculateDate(x,dayOffset):
   start_date =  datetime.datetime.strptime(x[1][:10], "%Y-%m-%d")
   return (start_date + datetime.timedelta(days=dayOffset)).strftime("%Y-%m-%d")
+def handleTuple(mappedTuple):
+  if len(mappedTuple) >1:
+    return (mappedTuple[0],json.loads(mappedTuple[1]))
+
+def toCSVLine(data):
+  return ','.join(str(d) for d in data)
 
 if len(sys.argv) < 2:
     sys.exit(0)
@@ -30,24 +36,26 @@ items = sc.broadcast([
 ])
 
 for item in items.value:
-  unique_ids = [sc.textFile('hdfs:///data/share/bdm/core-places-nyc.csv') \
+  unique_ids = sc.textFile('core_poi_ny.csv') \
     .map(lambda x: x.split(',')) \
     .map(lambda x: (x[1],x[9])) \
     .filter(lambda x: (x[1] in item[1])) \
-    .map(lambda x: x[0]) \
-    .distinct() \
-    .collect()]
+    .map(lambda x: (x[0],1)) \
+    .distinct() 
   
-  patterns = sc.textFile('hdfs:///data/share/bdm/weekly-patterns-nyc-2019-2020/*',use_unicode=False) \
+  loaddata = sc.textFile('hdfs:///data/share/bdm/weekly-patterns-nyc-2019-2020/*',use_unicode=False) \
     .map(lambda x: next(csv.reader([x]))) \
-    .filter(lambda x: (x[1] in unique_ids)) \
-    .map(lambda x: (x[1],x[12],json.loads(x[16]))) \
-    .flatMap(lambda x:  [(calculateDate(x,index),[visit])  for  index,visit in enumerate(x[2])] ) \
-    .reduceByKey(lambda a,b: a+b) \
-    .map(lambda x: (x[0][:4],x[0],int(np.median(x[1])),min(x[1]),max(x[1]))) \
-    .sortBy(lambda x:x[1])
-  if  not patterns.isEmpty():
-    sqlContext = SQLContext(sc)
-    df = sqlContext.createDataFrame(patterns,['year','date','median','low','high'])
+    .map(lambda x: (x[1],(x[12],x[16])))
+  
+  combined = unique_ids.join(loaddata) \
+    .map(lambda x: x[1][1]) \
+    .map(handleTuple) \
+    .flatMap(lambda x:  [(calculateDate(x,index),visit)  for  index,visit in enumerate(x[1])] ) \
+    .groupByKey() \
+    .map(lambda x: (x[0],list(x[1]))) \
+    .map(lambda x: (x[0][0:4],x[0],x[1])) \
+    .map(lambda x: (x[0],x[1],min(x[2]),max(x[2]),np.median(x[2])))
+    .map(toCSVLine)
     
-    df.write.format("csv").option('header',True).mode('overwrite').save('{}/{}.csv'.format(sys.argv[1],item[0]))
+  header = sc.parallelize(['year','date','min','max'])
+  header.union(combined).saveAsTextFile('{}/{}/{}.csv'.format(sys.argv[1],item[0],item[0]))  
